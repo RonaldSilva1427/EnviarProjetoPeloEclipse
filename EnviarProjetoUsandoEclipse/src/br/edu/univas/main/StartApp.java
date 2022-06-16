bf5b238782d24f391d5a5becceb8ccd085192b18
Ronald Pereira Da Silva <ronald.psilva1427@gmail.com>
2022-06-16 14:06:48 -0300

    Commit inicial

---
+++ EnviarProjetoUsandoEclipse/src/br/edu/univas/main/StartApp.java
@@ -0,0 +1,11 @@
+package br.edu.univas.main;
+
+public class StartApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Teste de envio de projeto pelo Git do Eclipse");
+
+	}
+
+}
