85ec16294f12fbc84a291f20a17f4a37a64880c9
Ronald Pereira Da Silva <ronald.psilva1427@gmail.com>
2022-06-16 14:09:25 -0300

    Modificado

--- EnviarProjetoUsandoEclipse/src/br/edu/univas/main/StartApp.java
+++ EnviarProjetoUsandoEclipse/src/br/edu/univas/main/StartApp.java
@@ -5,7 +5,8 @@ public class StartApp {
 	public static void main(String[] args) {
 
 		System.out.println("Teste de envio de projeto pelo Git do Eclipse");
-
+		System.out.println("Teste de envio de projeto pelo Git do Eclipse");
+		System.out.println("Teste de envio de projeto pelo Git do Eclipse");
 	}
 
 }
